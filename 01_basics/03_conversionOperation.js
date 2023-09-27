922592d8e1ec8f7679ed7fe0a9e04e21c41f9aa7
Faiz Alam <70325393+itsfaizalam@users.noreply.github.com>
2023-09-27 18:30:50 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,7 +31,65 @@ let isLoggedIn = "faiz"
 // ----------------------
 let someNumber =  33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
 
 // Below notes are tested output to understand the "String" conversions :-
 // 33 => 33 (but as a string)
+
+// ********** OPERATIONS *************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) // Two times multiply sign means its 2 to the power 3.
+// console.log(2/3)
+// console.log(2%3)  // To show the  remainder of 2 is to 3.
+
+let str1 = "Hello"
+let str2 = " Faiz"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); // if the first number is string then the output as string
+// console.log(1 + 2 + "2"); // if the last number is string then the output number and last on is as string
+
+// console.log((3 + 4) * 5 % 3);
+
+// These are the tricky conversion we should avoid this.
+// console.log(true); // output => true
+// console.log(+true); // output => 1
+// console.log(+""); // output => 0
+
+// Smart way to asign same value to multiple variables but avoid this in production line.
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+// console.log(num1 + num2 + num3);
+
+
+// Postfix and prefix Increment operations
+let gameCounter = 100
+//gameCounter++;
+++gameCounter;
+//console.log(gameCounter);
+
+
+// Example Source link : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+let x = 3;
+const y = x++;
+
+//console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+//console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+// Link to study Origianl JS Documentation https://tc39.es/ecma262/
