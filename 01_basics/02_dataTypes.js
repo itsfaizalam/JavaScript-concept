5bf6c9c4058fc7586bfce3c52a42f55e5232171f
Faiz Alam <70325393+itsfaizalam@users.noreply.github.com>
2023-09-27 10:55:45 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version of JS
+
+// alert( 3 + 3) // we are using nodejs, not browser.
+
+console.log(3 
+    + 
+    3) // code readability should be high
+
+console.log("Faiz")
+
+let name = "faiz"
+let age = 31
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bignint => to store large digit nunmber
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof null) // output is object
+console.log(typeof undefined); // output is undefined
